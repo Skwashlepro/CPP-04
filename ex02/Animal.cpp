fb083bf0218d32ef0332a7eebe5cb733a516a5d6
skwashlepro <skwashlepro@gmail.com>
2025-10-02 16:44:57 +0200

    aa

--- ex02/Animal.cpp
+++ ex02/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 18:05:24 by luctan            #+#    #+#             */
-/*   Updated: 2025/09/16 20:20:22 by luctan           ###   ########.fr       */
+/*   Updated: 2025/10/02 16:44:43 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,6 @@ Animal::Animal(std::string type) : type(type) {
 
 Animal::~Animal(){
 	std::cout << "Animal dead" << std::endl;
-}
-
-void	Animal::makeSound() const {
 }
 
 std::string Animal::getType() const {
